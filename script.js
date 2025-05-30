b414d610087d4c081a22d2a06a9fdb5a7465ba0c
Artem <artem.23.cz@gmail.com>
2025-05-30 20:28:19 +0900

    first commit

--- script.js
+++ script.js
@@ -1,71 +1,81 @@
-const search = document.getElementById('search');
-const autocomplete = document.getElementById('autocomplete');
-const repos = document.getElementById('repos');
-
-let timeout;
-search.addEventListener('input', function() {
-    clearTimeout(timeout);
-    const query = this.value.trim();
-            
-    if (!query) {
-        autocomplete.style.display = 'none';
-        return;
-        }
-
-        timeout = setTimeout(() => {
-            fetch(`https://api.github.com/search/repositories?q=${query}&per_page=5`)
-            .then(response => {
-                if (!response.ok) throw new Error('Network error');
-                    return response.json();
-                })
-            .then(data => {
-                showAutocomplete(data.items);
-                })
-            .catch(error => {
-                console.error('Error:', error);
-                autocomplete.style.display = 'none';
-                });
-            }, 300);
-        });
+const usernameInput = document.getElementById('username');
+const passwordInput = document.getElementById('password');
+const infoText = document.getElementById('popup_info')
+const button = document.getElementById('button');
+const info = document.getElementById('info_button');
+const infoButton = document.getElementById('info-button');
+const popup = document.getElementById('popup');
+const closeBtn = popup.querySelector('.popup__close');
+const API_URL = 'https://dummyjson.com/auth';
+let accessToken = null;
 
 
-function showAutocomplete(items) {
-    autocomplete.innerHTML = '';
-            
-    if (items && items.length > 0) {
-        items.forEach(item => {
-        const div = document.createElement('div');
-        div.className = 'autocomplete-item';
-        div.textContent = item.full_name;
-        div.onclick = () => addRepo(item);
-        autocomplete.appendChild(div);
-    });
-        autocomplete.style.display = 'block';
-        } else {
-                autocomplete.style.display = 'none';
+async function postInfo(usernameValue, passwordValue) {
+   const result = await fetch(`${API_URL}/login`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            username: usernameValue,
+            password: passwordValue,
+        }),
+    
+    }) 
+    const data = await result.json()
+    console.log('res: ', data)
+    accessToken = data.accessToken;
+
+    console.log(accessToken);
+}
+
+async function fetchUserData() {
+        try {
+            const response = await fetch(`${API_URL}/me`, {
+                method: 'GET',
+                headers: {
+                    'Authorization': `Bearer ${accessToken}`,
+                }
+            });
+
+            const data = await response.json();
+
+            if (!response.ok) {
+                throw new Error(data.message || 'Failed to fetch user data');
             }
-        }
 
-function addRepo(repo) {
-    const repoDiv = document.createElement('div');
-    repoDiv.className = 'repo-item';
-    repoDiv.innerHTML = `
-        <div class="repo-info">
-        <div class="repo-name">${repo.name}</div>
-        <div class="repo-meta">
-            <span>Owner: ${repo.owner.login}</span>
-            <span>Stars: ${repo.stargazers_count}</span>
-                    </div>
-            </div>
-            <span class="remove-btn" onclick="this.parentElement.remove()">✕</span>
-            `;
-            repos.appendChild(repoDiv);
-            search.value = '';
-            autocomplete.style.display = 'none';
+            return data;
+        } catch (error) {
+            throw new Error('Failed to fetch user information');
         }
+    }
 
-document.addEventListener('click', function(e) {
-    if (e.target !== search && !autocomplete.contains(e.target)) {
-    autocomplete.style.display = 'none';
-            }
+
+button.addEventListener('click', () => {
+    const loginData = postInfo(username.value, password.value);
+
+    info.style.display = "block";
+    
     });
+
+infoButton.addEventListener('click',() => {
+    let firstName;
+    popup.style.display = 'block';
+    fetchUserData().then(result => {
+        console.log(result.firstName);
+        firstName = result.firstName;
+        // infoText.innerText = `First Name : ${result.firstName} `
+        
+    }) 
+    infoText.innerText = `First Name : ${firstName} `
+});
+
+closeBtn.addEventListener('click', () => {
+    popup.style.display = 'none';
+});
+
+popup.addEventListener('click', (e) => {
+  if (e.target === popup) {
+    popup.style.display = 'none';
+  }
+});
